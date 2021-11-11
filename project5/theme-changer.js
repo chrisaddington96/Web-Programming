5e7e7f4a4f1c8aec0092dc58e12b375e4f59af63
chrisaddington96 <47895366+chrisaddington96@users.noreply.github.com>
2021-11-11 16:36:39 -0500

    Fixed bug
    Theme now saved to local storage.

--- project5/theme-changer.js
+++ project5/theme-changer.js
@@ -1,6 +1,43 @@
 // Function to change the theme
 function changeTheme(givenTheme){
     if(givenTheme =='dark'){
+        window.localStorage.setItem('theme', 'dark');
+        document.documentElement.style.setProperty("--background", "#161b22");
+        document.documentElement.style.setProperty("--brdr", "#30363d");
+        document.documentElement.style.setProperty("--txt", "#ffffff");
+        document.documentElement.style.setProperty("--background2", "#0d1117");
+    }
+    else if(givenTheme == 'light'){
+        window.localStorage.setItem('theme', 'light');
+        document.documentElement.style.setProperty("--background", "#ffffff");
+        document.documentElement.style.setProperty("--brdr", "#30363d");
+        document.documentElement.style.setProperty("--txt", "#4b0082");   
+        document.documentElement.style.setProperty("--background2", "#fffaf0");
+    }
+    else if(givenTheme == 'red'){
+        window.localStorage.setItem('theme', 'red');
+        document.documentElement.style.setProperty("--background", "#ffdead");
+        document.documentElement.style.setProperty("--brdr", "#30363d");
+        document.documentElement.style.setProperty("--txt", "#161b22");
+        document.documentElement.style.setProperty("--background2", "#ff7f50");
+        document.documentElement.style.setProperty("--bartxt", "#161b22");
+    }
+    else if(givenTheme == 'blue'){
+        window.localStorage.setItem('theme', 'blue');
+        document.documentElement.style.setProperty("--background", "#f0fff0");
+        document.documentElement.style.setProperty("--brdr", "#30363d");
+        document.documentElement.style.setProperty("--txt", "#161b22");
+        document.documentElement.style.setProperty("--background2", "#6495ed");
+    }
+    else{
+        alert(givenTheme + " is not an applicable color");
+    }
+}
+
+(function(){
+    var givenTheme = window.localStorage.getItem('theme');
+
+    if(givenTheme =='dark'){
         document.documentElement.style.setProperty("--background", "#161b22");
         document.documentElement.style.setProperty("--brdr", "#30363d");
         document.documentElement.style.setProperty("--txt", "#ffffff");
@@ -25,7 +62,4 @@ function changeTheme(givenTheme){
         document.documentElement.style.setProperty("--txt", "#161b22");
         document.documentElement.style.setProperty("--background2", "#6495ed");
     }
-    else{
-        alert(givenTheme + " is not an applicable color");
-    }
-}
+})();
