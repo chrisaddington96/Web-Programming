26cc6ccf721cb3793c075d1e77dade2a637cdf69
killowatt <killowatt2213@gmail.com>
2021-12-07 22:55:52 -0500

    Add working leaderboard search without page reload

--- project6/leaderboard-search.php
+++ project6/leaderboard-search.php
@@ -12,14 +12,14 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$results = $conn->query("SELECT fname, lname, networth FROM mydb ORDER BY networth DESC");
-while($row = $results->fetch_row()) {
-    printf("<tr>");
-    printf("<td>%s</td>", $row[0]);
-    printf("<td>%s</td>", $row[1]);
-    printf("<td>%s</td>", $row[2]);
-    printf("</tr>");
+$query = "SELECT fname, lname, networth FROM mydb ORDER BY networth DESC";
+if (!empty($_POST["search"])) {
+    $query = "SELECT fname, lname, networth FROM mydb WHERE lname LIKE '" . $_POST["search"] . "' ORDER BY networth DESC";
+    $_POST = array();
 }
+
+$results = $conn->query($query);
+echo json_encode($results->fetch_all());
 
 // Close the connection
 mysqli_close($conn);
