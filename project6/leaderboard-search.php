781755df2cc57eb59a8ea34a284d45859f3a44df
killowatt <killowatt2213@gmail.com>
2021-12-07 23:03:46 -0500

    Code comments and cleanup for leaderboard

--- project6/leaderboard-search.php
+++ project6/leaderboard-search.php
@@ -12,12 +12,16 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Set our default query
 $query = "SELECT fname, lname, networth FROM mydb ORDER BY networth DESC";
+
+// If we have been provided a search term, modify our query...
 if (!empty($_POST["search"])) {
     $query = "SELECT fname, lname, networth FROM mydb WHERE lname LIKE '" . $_POST["search"] . "' ORDER BY networth DESC";
     $_POST = array();
 }
 
+// Get our results and pack them in a JSON
 $results = $conn->query($query);
 echo json_encode($results->fetch_all());
 
